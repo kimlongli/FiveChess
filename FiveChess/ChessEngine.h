23844013c2188301f8a6f00fae7379029a271a03
Jinlong Li <kimlongli@icloud.com>
2018-05-21 12:41:36 +0800

    把所有tab替换成四个空格

--- FiveChess/ChessEngine.h
+++ FiveChess/ChessEngine.h
@@ -6,59 +6,59 @@ using namespace std;
 
 namespace ChessEngine {
 
-	//位置结构体，行是x，列是y
-	struct Position {
-		int x;
-		int y;
-		int score;
-		Position() {}
-		Position(int x, int y) {
-			this->x = x;
-			this->y = y;
-			score = 0;
-		}
-		Position(int x, int y, int score) {
-			this->x = x;
-			this->y = y;
-			this->score = score;
-		}
-		bool operator <(const Position &pos) const {
-			if (score != pos.score) {
-				return score > pos.score;
-			}
-			if (x != pos.x) {
-				return x < pos.x;
-			}
-			else {
-				return y < pos.y;
-			}
-		}
-	};
+    //位置结构体，行是x，列是y
+    struct Position {
+        int x;
+        int y;
+        int score;
+        Position() {}
+        Position(int x, int y) {
+            this->x = x;
+            this->y = y;
+            score = 0;
+        }
+        Position(int x, int y, int score) {
+            this->x = x;
+            this->y = y;
+            this->score = score;
+        }
+        bool operator <(const Position &pos) const {
+            if (score != pos.score) {
+                return score > pos.score;
+            }
+            if (x != pos.x) {
+                return x < pos.x;
+            }
+            else {
+                return y < pos.y;
+            }
+        }
+    };
 
 
-	//在开始之前，一些初始化工作
-	void beforeStart();
+    //在开始之前，一些初始化工作
+    void beforeStart();
 
-	//判断是否是某一方赢了
-	int isSomeOneWin();
+    //判断是否是某一方赢了
+    int isSomeOneWin();
 
-	//悔棋
-	string takeBack();
+    //悔棋
+    string takeBack();
 
-	//清除之前的记录，重新开局
-	string reset(int role);
+    //清除之前的记录，重新开局
+    string reset(int role);
 
-	//重新设置层数
-	void setLevel(int level);
+    //重新设置层数
+    void setLevel(int level);
 
-	//取得刚才电脑下得那一步棋子的位置
-	Position getLastPosition();
+    //取得刚才电脑下得那一步棋子的位置
+    Position getLastPosition();
 
-	//人类下棋，返回棋盘，传给界面
-	string nextStep(int x, int y);
+    //人类下棋，返回棋盘，传给界面
+    string nextStep(int x, int y);
 
-	//获取棋谱
-	vector<Position> getChessManual();
+    //获取棋谱
+    vector<Position> getChessManual();
 
 
 };
--- FiveChess/ChessManual.cpp
+++ FiveChess/ChessManual.cpp
@@ -4,120 +4,120 @@
 #include <fstream>
 
 ChessManual::ChessManual()
-	:whichFirst(0),
-	currentIndex(-1)
+    :whichFirst(0),
+    currentIndex(-1)
 {
-	int i = 0;
-	for (i = 0; i < 15 * 15; i++) {
-		board.push_back('0');
-	}
+    int i = 0;
+    for (i = 0; i < 15 * 15; i++) {
+        board.push_back('0');
+    }
 }
 
 ChessManual::ChessManual(const vector<ChessEngine::Position>& v, int whichFirst)
-	:whichFirst(whichFirst),
-	currentIndex(-1),
-	moves(v)
+    :whichFirst(whichFirst),
+    currentIndex(-1),
+    moves(v)
 {
-	int i = 0;
-	for (i = 0; i < 15 * 15; i++) {
-		board.push_back('0');
-	}
+    int i = 0;
+    for (i = 0; i < 15 * 15; i++) {
+        board.push_back('0');
+    }
 }
 
 
 bool ChessManual::ReadAndCheck(TCHAR *filename) {
-	fstream fs(filename);
+    fstream fs(filename);
 
-	//读入白字还是黑子先下
-	fs >> whichFirst;
-	if (whichFirst)
-		currentRole = '2';
-	else
-		currentRole = '1';
+    //读入白字还是黑子先下
+    fs >> whichFirst;
+    if (whichFirst)
+        currentRole = '2';
+    else
+        currentRole = '1';
 
-	//初始化棋盘
-	int i, j;
-	for (i = 0; i < 15; i++) {
-		for (j = 0; j < 15; j++) {
-			board[i * 15 + j] = '0';
-		}
-	}
+    //初始化棋盘
+    int i, j;
+    for (i = 0; i < 15; i++) {
+        for (j = 0; j < 15; j++) {
+            board[i * 15 + j] = '0';
+        }
+    }
 
-	int x = -1, y = -1;
-	while (fs >> x && fs >> y) {
-		//判断x，y是否合法
-		if (x >= 0 && x <= 14 && y >= 0 && y <= 14) {
-			moves.push_back(ChessEngine::Position(x, y));
-			if (board[x * 15 + y] == '0')
-				board[x * 15 + y] = '1';
-			else
-				return false;
-		}
+    int x = -1, y = -1;
+    while (fs >> x && fs >> y) {
+        //判断x，y是否合法
+        if (x >= 0 && x <= 14 && y >= 0 && y <= 14) {
+            moves.push_back(ChessEngine::Position(x, y));
+            if (board[x * 15 + y] == '0')
+                board[x * 15 + y] = '1';
+            else
+                return false;
+        }
 
-		x = -1, y = -1;
-	}
+        x = -1, y = -1;
+    }
 
-	//如果步数为0，返回false
-	if (!moves.size())
-		return false;
+    //如果步数为0，返回false
+    if (!moves.size())
+        return false;
 
-	//重置棋盘
-	for (i = 0; i < 15; i++) {
-		for (j = 0; j < 15; j++) {
-			board[i * 15 + j] = '0';
-		}
-	}
+    //重置棋盘
+    for (i = 0; i < 15; i++) {
+        for (j = 0; j < 15; j++) {
+            board[i * 15 + j] = '0';
+        }
+    }
 
-	fs.close();
-	return true;
+    fs.close();
+    return true;
 }
 
 string ChessManual::Next() {
-	if (currentIndex == moves.size() - 1)
-		return "";
+    if (currentIndex == moves.size() - 1)
+        return "";
 
-	currentIndex++;
-	ChessEngine::Position pos = moves[currentIndex];
+    currentIndex++;
+    ChessEngine::Position pos = moves[currentIndex];
 
-	//在棋盘中下子
-	board[pos.x * 15 + pos.y] = currentRole;
+    //在棋盘中下子
+    board[pos.x * 15 + pos.y] = currentRole;
 
-	//转换下子角色
-	changeRole();
+    //转换下子角色
+    changeRole();
 
-	return board;
+    return board;
 }
 
 string ChessManual::Previous() {
-	if (currentIndex == -1)
-		return "";
+    if (currentIndex == -1)
+        return "";
 
-	ChessEngine::Position pos = moves[currentIndex];
-	currentIndex--;
+    ChessEngine::Position pos = moves[currentIndex];
+    currentIndex--;
 
-	//在棋盘中去除此位置的棋子
-	board[pos.x * 15 + pos.y] = '0';
+    //在棋盘中去除此位置的棋子
+    board[pos.x * 15 + pos.y] = '0';
 
-	//转换下子角色
-	changeRole();
+    //转换下子角色
+    changeRole();
 
-	return board;
+    return board;
 }
 
 void ChessManual::SaveToFile(TCHAR *filename) {
-	fstream fs(filename, fstream::out);
+    fstream fs(filename, fstream::out);
 
-	//写谁先下
-	fs << whichFirst;
-	fs << '\n';
+    //写谁先下
+    fs << whichFirst;
+    fs << '\n';
 
-	//写棋谱
-	vector<ChessEngine::Position>::iterator iter;
-	for (iter = moves.begin(); iter != moves.end(); iter++) {
-		fs << (*iter).x << ' ' << (*iter).y << '\n';
-	}
+    //写棋谱
+    vector<ChessEngine::Position>::iterator iter;
+    for (iter = moves.begin(); iter != moves.end(); iter++) {
+        fs << (*iter).x << ' ' << (*iter).y << '\n';
+    }
 
-	fs.close();
+    fs.close();
 }
 
 ChessEngine::Position ChessManual::GetLastPosition() {
